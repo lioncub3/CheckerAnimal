9e4514732245abbaf8a96b1bc1a6c6b61327939d
lioncub3 <leonid2000.11.11@gmail.com>
2019-08-14 14:34:14 +0300

    update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,10 +30,8 @@
         {
             this.textBoxLink = new System.Windows.Forms.TextBox();
             this.buttonScan = new System.Windows.Forms.Button();
-            this.textBoxObjects = new System.Windows.Forms.TextBox();
-            this.textBoxTags = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBoxLink
@@ -53,53 +51,26 @@
             this.buttonScan.UseVisualStyleBackColor = true;
             this.buttonScan.Click += new System.EventHandler(this.ButtonScan_Click);
             // 
-            // textBoxObjects
+            // pictureBox1
             // 
-            this.textBoxObjects.Location = new System.Drawing.Point(28, 94);
-            this.textBoxObjects.Multiline = true;
-            this.textBoxObjects.Name = "textBoxObjects";
-            this.textBoxObjects.Size = new System.Drawing.Size(437, 55);
-            this.textBoxObjects.TabIndex = 2;
-            // 
-            // textBoxTags
-            // 
-            this.textBoxTags.Location = new System.Drawing.Point(28, 182);
-            this.textBoxTags.Multiline = true;
-            this.textBoxTags.Name = "textBoxTags";
-            this.textBoxTags.Size = new System.Drawing.Size(437, 72);
-            this.textBoxTags.TabIndex = 3;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(25, 78);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Objects";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(25, 166);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(31, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Tags";
+            this.pictureBox1.Location = new System.Drawing.Point(47, 82);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(486, 450);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBoxTags);
-            this.Controls.Add(this.textBoxObjects);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.buttonScan);
             this.Controls.Add(this.textBoxLink);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,10 +80,7 @@
 
         private System.Windows.Forms.TextBox textBoxLink;
         private System.Windows.Forms.Button buttonScan;
-        private System.Windows.Forms.TextBox textBoxObjects;
-        private System.Windows.Forms.TextBox textBoxTags;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -5,7 +5,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,7 +18,7 @@ namespace MyVisionApp
     {
 
         // subscriptionKey = "0123456789abcdef0123456789ABCDEF"
-        private const string subscriptionKey = "7801552db9cd438a8839d5a219efc8a0";
+        private const string subscriptionKey = "17aee88adf394be9b7d1141f805c49cb";
 
         // localImagePath = @"C:\Documents\LocalImage.jpg"
         private const string localImagePath = @"<LocalImage>";
@@ -39,11 +41,11 @@ namespace MyVisionApp
         {
             InitializeComponent();
 
-            computerVision = new ComputerVisionClient(new ApiKeyServiceClientCredentials(subscriptionKey), 
+            computerVision = new ComputerVisionClient(new ApiKeyServiceClientCredentials(subscriptionKey),
                 new System.Net.Http.DelegatingHandler[] { });
 
             // Specify the Azure region
-            computerVision.Endpoint = "https://mytest-vision.cognitiveservices.azure.com/";
+            computerVision.Endpoint = "https://superchecker.cognitiveservices.azure.com/";
         }
 
         // Analyze a remote image
@@ -62,11 +64,79 @@ namespace MyVisionApp
         // Display the most relevant caption for the image
         private void DisplayResults(ImageAnalysis analysis, string imageUri)
         {
-            if (analysis.Objects.Any(o => o.Parent.Parent.Parent.ObjectProperty == "animal"))
+            WebClient wc = new WebClient();
+            byte[] bytes = wc.DownloadData(imageUri);
+            MemoryStream ms = new MemoryStream(bytes);
+            Image img = Image.FromStream(ms);
+            ms.Close();
+            if (analysis.Objects.Any(o => o.ObjectProperty == "animal"))
+            {
                 MessageBox.Show("Увага тварина!");
+                using (Graphics g = Graphics.FromImage(img))
+                {
+                    BoundingRect animalRectangle = analysis.Objects.First(o => o.ObjectProperty == "animal").Rectangle;
+                    g.DrawRectangle(new Pen(Color.Red), new Rectangle(animalRectangle.X, animalRectangle.Y, animalRectangle.W, animalRectangle.H));
+                }
+            }
             else
-                MessageBox.Show("Усе безпечно");
+            {
+                if (analysis.Objects.Any(o => o.Parent != null))
+                {
+                    List<DetectedObject> childObjects = analysis.Objects.Where(o => o.Parent != null).ToList();
+                    List<ObjectHierarchy> parentObject = new List<ObjectHierarchy>();
+                    foreach (var detectedObject in childObjects)
+                    {
+                        parentObject.Add(detectedObject.Parent);
+                    }
 
+                    if (checkedAnimal(parentObject))
+                    {
+                        MessageBox.Show("Увага тварина!");
+
+                        using (Graphics g = Graphics.FromImage(img))
+                        {
+                            List<DetectedObject> animalObjects = new List<DetectedObject>();
+
+
+                            foreach (var item in childObjects)
+                            {
+                                if (checkedAnimal(new List<ObjectHierarchy>() { item.Parent }))
+                                {
+                                    BoundingRect animalRectangle = item.Rectangle;
+                                    g.DrawRectangle(new Pen(Color.Red), new Rectangle(animalRectangle.X, animalRectangle.Y, animalRectangle.W, animalRectangle.H));
+                                }
+                            }
+                        }
+                    }
+                    else
+                        MessageBox.Show("Усе безпечно");
+                }
+            }
+
+            pictureBox1.Image = img;
+        }
+
+        private bool checkedAnimal(List<ObjectHierarchy> objects)
+        {
+            if (objects.Any(o => o.ObjectProperty == "animal"))
+            {
+                return true;
+            }
+            else
+            {
+                if (objects.Any(o => o.Parent != null))
+                {
+                    List<ObjectHierarchy> childObjects = objects.Where(o => o.Parent != null).ToList();
+                    List<ObjectHierarchy> parentObject = new List<ObjectHierarchy>();
+                    foreach (var detectedObject in childObjects)
+                    {
+                        parentObject.Add(detectedObject.Parent);
+                    }
+                    if (checkedAnimal(parentObject))
+                        return true;
+                }
+                return false;
+            }
         }
 
         private async void ButtonScan_Click(object sender, EventArgs e)
